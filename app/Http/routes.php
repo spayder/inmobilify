f2a18422a2421e60f41d4b70581c2e4801e340ca
Piotr <gawronski.p@gmail.com>
2016-02-27 21:48:33 +0100

    Basic settings - gulp, first routes, etc

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,12 +8,6 @@
 | Here is where you will register all of the routes in an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +21,7 @@ Route::get('/', function () {
 */
 
 Route::group(['middleware' => ['web']], function () {
-    //
+    Route::get('/', function () {
+        return view('pages.home');
+    });
 });
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Inmobilify</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
